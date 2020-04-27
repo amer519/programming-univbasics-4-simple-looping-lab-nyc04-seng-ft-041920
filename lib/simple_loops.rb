507548624c3b13d83f33bc7c035c430d22754308
Amer Fahmy <afahmy519@gmail.com>
2020-04-27 23:01:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -29,8 +29,9 @@ end
 
 def return_string_array (array)
   count = 0
+  new_array = [ ]
   while count < array.length do
-    puts array [count].to_s
+    puts new_array << array [count].to_s
     count += 1
   end
   new_array
