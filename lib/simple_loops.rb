cab46bfb77323057066837b9b5905077b7613a36
Amer Fahmy <afahmy519@gmail.com>
2020-04-27 22:46:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,12 @@ def loop_message_five_times (message)
    count += 1
  end
 end
+
+def loop_message_n_times (message, integer)
+  
+   count = 0
+  while == integer
+ puts message
+   count +=1
+  end
+end
